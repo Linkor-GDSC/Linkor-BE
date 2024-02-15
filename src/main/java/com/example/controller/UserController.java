73dfc4079dd861131298ed1d9781b92309399696
jiwonniy <jiwoni0707@gmail.com>
2024-02-15 22:56:07 +0900

    [Refactor] change return-type to Response

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.domain.user.User;
 import com.example.dto.TimeDto;
 import com.example.dto.UserAndTimeDto;
 import com.example.dto.UserDto;
+import com.example.response.Response;
 import com.example.service.TimeServiceImpl;
 import com.example.service.UserServiceImpl;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -26,22 +27,12 @@ public class UserController {
         this.userService = userService;
         this.timeService = timeService;
     }
-//    @PostMapping("register")
-//    //@ApiOperation(value = "유저 회원등록", notes = "유저 정보를 입력받아 회원등록을 하고, 이미 등록된 유저이면 등록하지 않는다.")
-//    public void register(@RequestBody UserDto user){
-//        try{
-//            System.out.println(user.toString());
-//            userService.join(user);
-//        }catch (DataIntegrityViolationException e){
-//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미 등록된 회원입니다.");
-//        }
-//    }
 
+    //회원 등록
     @PostMapping("register")
     public boolean register(@RequestBody UserDto user){
         try{
             if (userService.checkUserDuplicate(user.getEmail())){
-                //bindingResult.addError(new FieldError("SignUp Request", "email", "이미 등록된 회원입니다."));
                 return true;
             }else{
                 System.out.println(user.toString());
@@ -63,7 +54,7 @@ public class UserController {
 
     //time 저장
     @PostMapping("register/time")
-    public void register_time(@RequestBody TimeRequest request) {
+    public Response register_time(@RequestBody TimeRequest request) {
         String email = request.getEmail();
         List<String> times = request.getTimes();
 
@@ -72,26 +63,37 @@ public class UserController {
 
             try {
                 timeService.save(timeDto);
+                return new Response("성공", "요일 저장 성공", null);
             } catch (DataIntegrityViolationException e) {
-                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미 등록된 회원입니다.");
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미 등록된 시간입니다.");
             }
         }
+        return null;
     }
+
+//    @GetMapping("all")
+//    public List<User> findAll(){
+//        return userService.findUsers();
+//    }
+
+    //모든 회원 목록 조회 (요일 제외)
     @GetMapping("all")
-    //@ApiOperation(value = "전체 유저 정보 찾기", notes = "전체 유저 데이터를 찾아 리턴한다.")
-    public List<User> findAll(){
-        return userService.findUsers();
+    public Response findAll(){
+        return new Response("성공", "모든 회원목록 조회 성공",userService.findUsers());
     }
 
+    //모든 튜터 목록 조회 (요일 제외)
     @GetMapping("tutors")
-    public List<User> findTutors(String role){
-        return userService.findTutors(role);
+    public Response findTutors(String role){
+        return new Response("성공", "모든 튜터목록 조회 성공",userService.findTutors(role));
     }
 
+    //이메일로 튜터 정보 조회
     //@PathVariable : 경로 변수
     @GetMapping("tutors/{email}")
-    public UserAndTimeDto findTutorByEmail(@PathVariable String email){
-        return userService.findTutorByEmail(email);
+    public Response findTutorByEmail(@PathVariable String email){
+
+        return new Response("성공", "튜터정보 조회 성공", userService.findTutorByEmail(email));
     }
   /*
     //tutor 필터링
@@ -107,26 +109,30 @@ public class UserController {
 
    */
 
+    //필터링 결과에 따른 튜터 정보 조회
     @GetMapping("/filter")
-    public List<UserAndTimeDto> getUsersByFilterWithTime(
+    public Response getUsersByFilterWithTime(
             @RequestParam(value = "gender", required = false) String gender,
             @RequestParam(value = "locationsido", required = false) String locationsido,
             @RequestParam(value = "locationgu", required = false) String locationgu,
             @RequestParam(value = "tutoringmethod", required = false) String tutoringmethod,
             @RequestParam(value = "times", required = false) List<String> times) {
 
-        return userService.getUsersByFilterWithTime(gender, locationsido, locationgu, tutoringmethod, times);
+        return new Response("성공", "튜터 필터링 성공",
+                userService.getUsersByFilterWithTime(gender, locationsido, locationgu, tutoringmethod, times));
     }
 
+    //요일에 따른 튜터 조회
     @GetMapping("tutors/time/{time}")
-    public List<User> findTutorsByTime(@PathVariable String time){
-        return userService.findTutorsByTime(time);
+    public Response findTutorsByTime(@PathVariable String time){
+        return new Response("성공", "요일별 튜터 조회 성공", userService.findTutorsByTime(time));
     }
 
-    //유저 정보 검색
+    //유저 정보 검색 (요일 포함)
     @GetMapping("/{email}")
-    public UserAndTimeDto findUserByEmail(@PathVariable String email){
-        return userService.findUserByEmail(email);
+    public Response findUserByEmail(@PathVariable String email){
+
+        return new Response("성공", "요일별 회원 조회 성공", userService.findUserByEmail(email));
     }
 
     @PatchMapping("/updateIntro")
@@ -134,5 +140,50 @@ public class UserController {
             @RequestParam(value = "email") String email,
             @RequestParam(value = "intro") String introduction) {
         userService.updateUserIntro(email, introduction);
+    }
+
+    //마이페이지에서 정보 조회
+    @GetMapping("/mypage/{email}")
+    public Response getMypage3(@PathVariable String email){
+        return new Response("성공", "마이페이지 조회 성공", userService.findUserByEmail(email));
+    }
+
+    //마이페이지에서 정보 업데이트 (요일 제외)
+    @PatchMapping("/mypage/{email}/edit")
+    public Response editMypage(
+            @PathVariable String email,
+            @RequestBody UserDto userDto) {
+        userService.editMypage(email, userDto);
+        return new Response("성공", "마이페이지 업데이트 성공",null);
+    }
+
+    //마이페이지에서 요일 추가
+    @PostMapping("/mypage/edit-time")
+    public Response addMypageTime(@RequestBody TimeRequest request) {
+        String email = request.getEmail();
+        List<String> times = request.getTimes();
+
+        for (String time : times) {
+            TimeDto timeDto = new TimeDto(time, email);
+
+            try {
+                timeService.save(timeDto);
+                return new Response("성공", "요일 저장 성공", null);
+            } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미 저장된 요일입니다.");
+            }
+        }
+        return null;
+    }
+
+    //마이페이지에서 요일 삭제
+    @DeleteMapping("/mypage/delete-time")
+    public Response deleteMypageTime(@RequestBody TimeRequest request) {
+        String email = request.getEmail();
+        List<String> times = request.getTimes();
+        String onetime = times.get(0);
+
+        timeService.deleteByEmailAndTime(email, onetime);
+        return new Response("성공", "요일 삭제 성공", null);
     }
 }
